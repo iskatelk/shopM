ffb557d77fd53c1cfa741cd744a0124f1b30dcf4
Igor Kitaev <kitaev_igor62@mail.ru>
2023-07-07 21:45:22 +0300

    Исправить ошибку src/Controller/AccountController.php

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Order;
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
