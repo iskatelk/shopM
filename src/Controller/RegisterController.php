bc16112a94ae4065b323a774ae831a1805cccdd8
Igor Kitaev <kitaev_igor62@mail.ru>
2023-07-07 14:20:28 +0300

    Исправить RegisterController.php

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -4,9 +4,13 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class RegisterController extends AbstractController
 {
+    /**
+     * @Route("/register", name="app_register")
+     */
     public function register() : Response
     {
         return $this->render('register/register.html.twig', [
