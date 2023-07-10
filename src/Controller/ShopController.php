6fd862de5a6579c36d48efb1bfd864a32992dbd1
Igor Kitaev <kitaev_igor62@mail.ru>
2023-07-10 05:27:33 +0300

    Добавить контроллер и шаблон shop

---
+++ src/Controller/ShopController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ShopController extends AbstractController
+{
+    /**
+     * @Route("/shop", name="app_shop")
+     */
+    public function shop():Response
+    {
+
+        return $this->render('cart/cart.html.twig', [
+            'controller_name', 'ShopController',
+        ]);
+    }
+}
